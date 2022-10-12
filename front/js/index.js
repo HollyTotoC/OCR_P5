2f023c5eff1d634f3f5b0798091647603f7ba8e5
Théo-toto Certa <totoc.pro@gmail.com>
2022-10-12 23:21:24 +0200

    Creation de index.js
    script pour la page index.html permettant de loader les articles depuis l'API et afficher le contenue dans le DOM.

---
+++ front/js/index.js
@@ -0,0 +1,32 @@
+//Chargement des produits et affichage sur la page d'accueil
+let afficherProduits = async function () {
+    await fetch('http://localhost:3000/api/products')
+    .then(res => res.json())
+    .then ((data) => {
+        let product = data;
+        
+        /*
+        console.log(product); //vérification console de ce qui est renvoyé
+        console.log(product[3].name); //vérification de l'item 3
+        */
+
+        let ancreProduits = document.querySelector('#items'); //selection de l'id où insérer le catalogue
+        
+        for (i=0; i < product.length; i++) {
+            //Affichage des produits dans le DOM
+                    const ficheProduit = '<a href="./product.html?id='
+                            + product[i]._id 
+                            + '"><article><img src="'
+                            + product[i].imageUrl
+                            + '" alt="' 
+                            + product[i].altTxt
+                            +'" /><h3 class="productName">'
+                            + product[i].name + '</h3><p class="productDescription">'
+                            + product[i].description
+                            + '</p></article></a>';
+                    ancreProduits.innerHTML += ficheProduit;
+                }
+            });
+        };
+        
+        afficherProduits();
